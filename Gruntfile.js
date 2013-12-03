b8eb9ac64ee2268a0582810855b31328f763d70d
Dapeng Li <luislee818@gmail.com>
2013-12-03 22:43:31 +0800

    Update gruntfile after renaming source file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 // Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
 module.exports = function(grunt) {
- 
+
   // Load Grunt tasks declared in the package.json file
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
- 
-  // Configure Grunt 
+
+  // Configure Grunt
   grunt.initConfig({
- 
+
     // grunt-express will serve the files from the folders listed in `bases`
     // on specified `port` and `hostname`
     express: {
@@ -19,12 +19,12 @@ module.exports = function(grunt) {
         }
       }
     },
- 
+
     // grunt-watch will monitor the projects files
     watch: {
       all: {
         // Replace with whatever file you want to trigger the update from
-        // Either as a String for a single entry 
+        // Either as a String for a single entry
         // or an Array of String for multiple entries
         // You can use globing patterns like `css/**/*.css`
         // See https://github.com/gruntjs/grunt-contrib-watch#files
@@ -34,16 +34,16 @@ module.exports = function(grunt) {
         }
       }
     },
- 
+
     // grunt-open will open your browser at the project's URL
     open: {
       all: {
         // Gets the port from the connect configuration
-        path: 'http://localhost:<%= express.all.options.port%>/csharp.html'
+        path: 'http://localhost:<%= express.all.options.port%>/cups.cs.html'
       }
     }
   });
- 
+
   // Creates the `server` task
   grunt.registerTask('server', [
     'express',
